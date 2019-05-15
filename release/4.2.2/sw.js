e9a9307ad3dca06d8e619c893c27c94e7f47434e
netwixell <newaitix@gmail.com>
2019-05-15 10:37:20 +0300

    -

---
+++ release/4.2.2/sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install',function(event){
+	event.waitUntil(caches.open('my-site-cache-v1').then(function(cache){
+		return cache.addAll([
+			'/templater/popUpId.tpl',
+			'/templater/bodyId.tpl',
+			'/templater/topNavLog.tpl',
+			'/templater/topNav.tpl',
+			'/templater/bodyFailedAuth.tpl',
+			'/templater/registration.tpl',
+			'/templater/login.tpl',
+			'/templater/syncToServ.tpl',
+			'/templater/syncToPc.tpl',
+			'/templater/emptySearch.tpl',
+			'/templater/updateButton.tpl',
+			'/templater/notification.tpl',
+			'/js/conf.js',
+			'/js/library.js',
+			'/js/script.js',
+			'/js/sevice.js',
+			'/css/style.css'
+		]);
+	}));
+});
+self.addEventListener('fetch', function(event) {
+	event.respondWith(caches.match(event.request).then(function(response){
+		if (response)
+			return response;
+		return fetch(event.request);
+	}));
+});
